e0ef139e69bf7b60173c0788585bc4ed34134c88
LeonidShamarin <Leonideko1@gmail.com>
2023-03-20 12:35:12 +0000

    task4

---
+++ task4/script.js
@@ -0,0 +1,176 @@
+"use strict";
+//1 Напишіть функцію sumSliceArray(arr, first, second), яка приймає масив arr
+// і два числа (first и second) – порядкові номери елементів масиву, які необхідно
+//  скласти. Наприклад, якщо ввели 3 та 5 – сумуються 3-й та 5-й елементи.Функція повинна
+//   генерувати винятки, якщо були введені не числа, і коли одне з чисел або обидва більшого
+//   розміруза довжину масиву. Напишіть код, який використовує цю функцію, передбачте обробку
+//   можливих винятків.
+function sumSliceArray(arr, first, second) {
+    if (typeof first !== "number" || typeof second !== "number") {
+        throw new Error('Both "first" and "second" parameters should be numbers');
+    }
+    if (first < 0 || second < 0 || first >= arr.length || second >= arr.length) {
+        throw new RangeError("Index should be within the range of array length");
+    }
+    const sum = arr[first] + arr[second];
+    return sum;
+}
+
+const arr = [1, 2, 3, 4, 5];
+try {
+    console.log(sumSliceArray(arr, 2, "3"));
+} catch (error) {
+    console.log(`Error occurred: ${error.message}`);
+}
+try {
+    console.log(sumSliceArray(arr, -1, 3));
+} catch (error) {
+    console.log(`Error occurred : ${error.message}`);
+}
+try {
+    console.log(sumSliceArray(arr, 2, 6));
+} catch (error) {
+    console.log(`Error occurred : ${error.message}`);
+}
+
+console.log(sumSliceArray(arr, 1, 3));
+
+//2Створіть функцію checkAge(), яка запитує у користувача його ім'я, вік
+// та статус (адмін, модератор, користувач) та генерує модальне вікно з помилкою,
+//  якщо:вік користувача менше 18 або більше 70 років (генерується помилка типу
+//  RangeError).користувач не ввів жодних даних в будь-якому полі (виводиться
+//  повідомлення The field is empty! Please enter your age з типом помилки
+//  Error). У полі статус введено неправильне слово (тип помилки EvalError).
+//  полі вік введено нечислове значення. У всіх інших випадках користувач отримає
+//   доступ до перегляду фільму. У блоці catch передбачена можливість виведення назви
+//   та опису помилки.
+function checkAge() {
+    try {
+        let name = prompt("Enter your name:");
+        let age = prompt("Enter your age:");
+        let status = prompt("Enter your status (admin,moderator,user):");
+
+        if (!age) {
+            throw new Error("The field is empty! Please enter your age");
+        }
+        age = Number(age);
+        if (isNaN(age)) {
+            throw new TypeError("Invalid age value. Age must be a number.");
+        }
+        if (age < 18 || age > 70) {
+            throw new RangeError("Age must be between 18 and 70 years old.");
+        }
+        if (status !== "admin" && status !== "moderator" && status !== "user") {
+            throw new EvalError(
+                "Invalid status value. Status must be admin, moderator or user."
+            );
+        }
+        alert("You have acces to watch the movie.");
+    } catch (error) {
+        alert(`${error.name}: ${error.message}`);
+    }
+}
+checkAge();
+
+//3Реалізуйте функцію calcRectangleArea(width, height), яка приймає 2 параметри
+//  ширина прямокутника width і висота прямокутника height і обраховує його площу.
+//  Передбачити припинення виконання програми і генерацію винятку у випадку, якщо
+//  функції передано не числові параметри.
+// Напишіть код, який використовує цю функцію та обробляє можливі виняткові ситуації.
+function calcRectangleArea(width, height) {
+    if (typeof width !== "number" || typeof height !== "number") {
+        throw new Error("Both arguments must be numbers");
+    }
+    return width * height;
+}
+try {
+    const width = 10;
+    const height = "20";
+    const area = calcRectangleArea(width, height);
+    console.log(`Площа прямокутника: ${area}`);
+} catch (error) {
+    console.error(`Помилка: ${error.message}`);
+}
+
+//4 Створіть клас MonthException, конструктор якого приймає параметр message і ініціалізує поле
+// name значенням MonthException.
+// Реалізуйте функцію showMonthName(month), в якій параметр month – це порядковий номер місяця в
+//  році. Функція повертає назву місяця відповідно до введеного номера місяця. У випадку некоректного
+//  вводу кидається ексепшн у вигляді об’єкта класу MonthException з повідомленням Incorrect month number.
+// Напишіть код, який використовує цю функцію, передбачте обробку можливих винятків.
+// Приклад роботи програми:
+// console.log(showMonthName(5));
+// May
+// console.log(showMonthName(14));
+// MonthException Incorrect month number
+class MonthException extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "MonthException";
+    }
+}
+
+function showMonthName(month) {
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    if (month < 1 || month > 12) {
+        throw new MonthException("Incorrect month number");
+    }
+
+    return months[month - 1];
+}
+try {
+    console.log(showMonthName(5)); // May
+    console.log(showMonthName(14)); // викличе ексепшн
+} catch (e) {
+    if (e instanceof MonthException) {
+        console.log(e.name + " " + e.message); // MonthException Incorrect month number
+    } else {
+        throw e; // перекинути інший тип ексепшну далі
+    }
+}
+
+//5Реалізуйте функцію showUser(id), яка приймає параметром користувацьке id і
+// повертає об’єкт, який містить значення переданої id. Також функція викидає помилку
+// у разі якщо введено від’ємне id.
+// Реалізуйте функцію showUsers(ids), яка приймає параметром масив користувацьких айді
+//  ids, перевіряє з використанням функції showUser() кожен елемент масиву ids на коректність,
+//   в разі виключної ситуації виводить повідомлення про помилку. Функція showUsers(ids) повертає
+//   масив об’єктів, де значеннями ключів є коректні елементи ids.
+
+// Приклад роботи програми:
+// showUsers([7, -12, 44, 22]);
+// Error: ID must not be negative: -12
+// [ {id: 7}, {id: 44}, {id: 22} ]
+function showUser(id) {
+    if (id < 0) {
+        throw new Error("ID must not be negative: " + id);
+    }
+    return { id: id };
+}
+
+function showUsers(ids) {
+    var result = [];
+    for (var i = 0; i < ids.length; i++) {
+        try {
+            var user = showUser(ids[i]);
+            result.push(user);
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
+    return result;
+}
